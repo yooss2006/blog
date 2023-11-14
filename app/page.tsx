e1889cf27be120817200c8051314d058a06dcf06
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2023-11-14 22:18:43 +0900

    Feat: ESLint + Prettier + tsconfig 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <main>시작</main>
-  )
+  return <main>시작</main>;
 }
---
+++ prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  quoteProps: "consistent",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
